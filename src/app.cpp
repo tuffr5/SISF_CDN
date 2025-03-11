758da78fd55d2d29b4f6cce4ab9232854d43995a
Logan Walker <loganaw@umich.edu>
2025-03-10 22:52:46 -0400

    Update app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -37,7 +37,7 @@ bool READ_ONLY_MODE = false;
 
 std::string DATA_PATH = "./data/";
 std::string SERVER_ROOT = "https://server/";
-const std::string VERSION_STRING = "V0-6-0"
+const std::string VERSION_STRING = "V0-6-0";
 
 using json = nlohmann::json;
 using basic_json = nlohmann::json;
